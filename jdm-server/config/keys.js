8843223b8589dff54178dd02fe8f1ebd36401117
abdougenesis <fa_djellal@esi.dz>
2021-02-05 16:59:25 +0100

    added atlas bdd

--- jdm-server/config/keys.js
+++ jdm-server/config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoURI: "mongodb://localhost:27017/JDM",
+    mongoURI: process.env.MONGODB_URI || "mongodb://localhost:27017/JDM",
     API: "/api/",
     JDMServerXML:
         "http://www.jeuxdemots.org/rezo-xml.php?gotermsubmit=Chercher&output=onlyxml&gotermrel=",
